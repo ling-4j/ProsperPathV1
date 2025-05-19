147b4ec45fbf8e372a0feb80f39df0c271c32003
ling-4j <90211232+ling-4j@users.noreply.github.com>
2025-05-19 14:26:45 +0700

    setActived = false

--- src/main/java/com/mycompany/myapp/service/UserService.java
+++ src/main/java/com/mycompany/myapp/service/UserService.java
@@ -111,9 +111,7 @@ public class UserService {
         newUser.setImageUrl(userDTO.getImageUrl());
         newUser.setLangKey(userDTO.getLangKey());
         // new user is not active
-        // newUser.setActivated(false);
-
-        newUser.setActivated(true);
+        newUser.setActivated(false);
 
         // new user gets registration key
         newUser.setActivationKey(RandomUtil.generateActivationKey());
